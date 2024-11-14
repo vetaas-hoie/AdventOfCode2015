68a75cd7168b441c834b64425f633396fd6664f8
Asbjørn Vetaas-Høie <asbjorn.vetaas-hoie@capgemini.com>
2024-11-14 10:04:57 +0100

    Day1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,13 +12,41 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Reading File...");
+            string[] a = (string[])File.ReadAllLines("D:\\Git\\vetaas-hoie\\AdventOfCode2015\\text.txt");
 
-            Console.WriteLine("Reading File: ");
-            //string[] a = (string[])File.ReadAllLines("C:\\Users\\avetaash\\source\\repos\\AdventOfCode\\text.txt");
-            string[] a = (string[])File.ReadAllLines(Directory.GetCurrentDirectory() + "\\..\\..\\..\\text.txt");
-            Console.WriteLine("Total lines: " + a.Length);
+            Console.WriteLine("Ended up in floor: " + Day1(a[0]));
+            Console.WriteLine("First time in basement: " + Day1_2(a[0]));
+
 
             Console.ReadKey();
+        }
+
+        static int Day1(string a)
+        {
+            int floor = 0;
+
+            foreach (char c in a)
+            {
+
+                if (c == '(') floor++;
+                if (c == ')') floor--;
+
+            }
+            return floor;
+        }
+        static int Day1_2(string a)
+        {
+            int floor = 0;
+            int count = 0;
+            foreach (char c in a)
+            {
+                count++;
+                if (c == '(') floor++;
+                if (c == ')') floor--;
+                if (floor == -1) return count;   //added for part two 
+            }
+            return floor;
         }
 
 
